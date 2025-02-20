4ed9a059f72ea394c8b0a94c29ea3f9c4f407267
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-19 18:01:12 -0600

    Partially complete event delegation
    Attempting to use event delegation to only have
    to add an event listener to container, just not
    working yet.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ const container = document.querySelector(".container");
 const square = document.createElement("div");
 square.classList.add("gridSquare");
 
+let i = 0;
+container.addEventListener("mouseenter", (event) => {
+    console.log(event.target)
+    event.target.classList.add("hovered");
+    console.log(++i);
+})
+
 const gridSize = 16 * 16;
 for (let i = 0; i < gridSize; i++) {
     container.appendChild(square.cloneNode(true));
