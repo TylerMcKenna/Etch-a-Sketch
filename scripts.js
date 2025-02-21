9052bc2fc6ce85c49497b57539e8b4ce30c01637
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-20 18:14:16 -0600

    Finish Project

--- scripts.js
+++ scripts.js
@@ -1,15 +1,35 @@
-const container = document.querySelector(".container");
-const square = document.createElement("div");
-square.classList.add("gridSquare");
+document.addEventListener("DOMContentLoaded", initialize());
 
-let i = 0;
-container.addEventListener("mouseenter", (event) => {
-    console.log(event.target)
-    event.target.classList.add("hovered");
-    console.log(++i);
-})
+function createGrid(sideLength) {   
+    container = document.querySelector(".container");
+    container.innerHTML = "";
 
-const gridSize = 16 * 16;
-for (let i = 0; i < gridSize; i++) {
-    container.appendChild(square.cloneNode(true));
+    const square = document.createElement("div");
+    square.style.padding = `${100/(2 * sideLength)}%`;
+    square.classList.add("gridSquare");
+
+    const gridSize = sideLength * sideLength;
+    for (let i = 0; i < gridSize; i++) {
+        container.appendChild(square.cloneNode(true));
+    }
+
+    container.addEventListener("mouseover", (event) => {
+        const target = event.target;
+        target.classList.add("hovered");
+    })
+}
+
+function initialize() {
+    createGrid(16);
+    
+    const sizeButton = document.querySelector(".sizeButton");
+    sizeButton.addEventListener("click", promptNewSize); 
+}
+
+function promptNewSize() {
+    let input = parseInt(prompt("Please enter the new size for the grid's side length."));
+    while(isNaN(input) || input > 99 || input < 1) {
+        input = parseInt(prompt("Please make sure to input a number, less than 100."));
+    }
+    createGrid(input);
 }
